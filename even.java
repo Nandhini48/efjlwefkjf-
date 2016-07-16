f3beeea6b4e619ad92a33443f679e89abe447cb0
Nandhini48 <nandhininandhuas@gmail.com>
2016-07-16 02:10:32 -0700

    Add files via upload

---
+++ even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOrOdd{
+         public static void main(String[] args) {
+         int n;
+         System.out.println("enter the number to check");
+         Scanner a=new Scanner(System.in);
+         n=a.nextInt();
+         if(n%2==0){
+             System.out.println("even number");
+            }
+         else
+                 System.out.println("odd number");
+      }
+    
+    }
+
